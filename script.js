eb401291ca61697337bf724e639393b842643ff2
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-31 20:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function toCase(text) {
-  // write your code here
+  // Check if the input string is empty
+  if (!text) {
+    return '-';
+  }
+
+  // Convert the string to lowercase and uppercase
+  const lowercaseText = text.toLowerCase();
+  const uppercaseText = text.toUpperCase();
+
+  // Concatenate the strings with '-' as the delimiter
+  const result = `${lowercaseText}-${uppercaseText}`;
+
+  return result;
 }
 
-// DO not change the code below
-
+// Get user input using prompt
 const text = prompt("Enter text:");
+
+// Display the result using alert
 alert(toCase(text));
